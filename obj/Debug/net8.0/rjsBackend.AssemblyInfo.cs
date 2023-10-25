354cd3b639fc2b3ac70adb1bc56c271c0edb08f5
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2023-10-25 22:46:34 +0100

    mac git ignore

--- obj/Debug/net8.0/rjsBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/rjsBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rjsBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2b30f3d69470cf02b57635873386ab0dd5ea94")]
 [assembly: System.Reflection.AssemblyProductAttribute("rjsBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rjsBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
